ca96f482fa8c247e4c331a7634542e881bd3e9fa
osirisgenius <59709335+osirisgenius@users.noreply.github.com>
2021-01-26 05:19:48 +0800

    List Update see on Description
    1.) Added Click Me
    2.) Add Hover on circle
    3.) Change this to my portfolio

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// function insertGuest(){
+//     const yourName = prompt("Enter Your Name").toUpperCase()
+//     const firstLetter = yourName[0]
+//     const remainingLetter = yourName.slice(1).toLocaleLowerCase()
+//     const insertName = document.querySelector(".t-span")
+
+//     if (yourName == ""){
+//         insertName.innerHTML =  "Guest!";
+//     }
+
+//     else{
+//         insertName.innerHTML =  firstLetter + remainingLetter + '!';
+//     }
+// }
+
+// insertGuest()
