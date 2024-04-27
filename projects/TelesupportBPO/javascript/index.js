8e447e9655271603f262a2dc55699ea21f1059ab
Niño Magampon <82831506+niniomagampon@users.noreply.github.com>
2024-04-27 16:58:01 +0800

    Fix submit form error

--- projects/TelesupportBPO/javascript/index.js
+++ projects/TelesupportBPO/javascript/index.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
     var video = document.getElementById('myVideo');
 
     video.addEventListener('ended', function () {
-        // Rewind the video to the beginning
+
         this.currentTime = 0;
-        // Play the video again
+
         this.play();
     });
 });
@@ -14,12 +14,13 @@ var recaptcha_response = ""
 const captchaForm = document.getElementById("captchaFormID")
 const captchaResult = document.getElementById("g-recaptcha-error")
 
-function submitSendMessageForm(e) {
-    e.preventDefault()
+function submitSendMessageForm(event){
+    event.preventDefault()
 
     if(recaptcha_response.length == 0) {
         captchaResult.innerHTML = '<span style="color:red;">This field is required.</span>';
         return false;
     }
-    return true;
+
+    document.getElementById('g-recaptcha-error').innerHTML = ''
 }
