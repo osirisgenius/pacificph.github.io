2fa3b7f7a14c2da33d4e50cff2b6d103612d7a3d
Niño Magampon <82831506+niniomagampon@users.noreply.github.com>
2024-04-27 17:38:19 +0800

    update index js

--- projects/TelesupportBPO/javascript/index.js
+++ projects/TelesupportBPO/javascript/index.js
@@ -14,17 +14,45 @@ const captchaResult = document.getElementById("g-recaptcha-error")
 
 var recaptcha_response = ''
 
-function submitSendMessageForm(event){
-    event.preventDefault()
+// function submitSendMessageForm(event){
+//     event.preventDefault()
 
-    if(recaptcha_response.length == 0) {
+//     if(recaptcha_response.length == 0) {
+//         captchaResult.innerHTML = '<span style="color:red;">This field is required.</span>';
+
+//         return false;
+//     }
+// }
+
+// function verifyCaptcha(token) {
+//     recaptcha_response = token;
+//     document.getElementById('g-recaptcha-error').innerHTML = '';
+// }
+
+// Function to handle reCAPTCHA callback
+function handleRecaptchaResponse(response) {
+    recaptcha_response = response;
+}
+
+// Callback function for reCAPTCHA widget
+function onSubmitRecaptcha(token) {
+    handleRecaptchaResponse(token);
+}
+
+// Function to handle form submission
+function submitSendMessageForm(event) {
+    event.preventDefault();
+
+    if (recaptcha_response.length === 0) {
         captchaResult.innerHTML = '<span style="color:red;">This field is required.</span>';
-
+        console.log("captcha: " + recaptcha_response);
         return false;
     }
-}
 
-function verifyCaptcha(token) {
-    recaptcha_response = token;
+    console.log("captcha: " + recaptcha_response);
+
     document.getElementById('g-recaptcha-error').innerHTML = '';
 }
+
+// Event listener for form submission
+captchaForm.addEventListener('submit', submitSendMessageForm);
